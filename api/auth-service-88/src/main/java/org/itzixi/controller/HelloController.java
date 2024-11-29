c509dc028deef99d3cd9dc3bf9150602acd72f1a
Donkor <donkor@yeah.net>
2024-11-29 11:32:24 +0800

    腾讯云发送短信

--- api/auth-service-88/src/main/java/org/itzixi/controller/HelloController.java
+++ api/auth-service-88/src/main/java/org/itzixi/controller/HelloController.java
@@ -1,5 +1,7 @@
 package org.itzixi.controller;
 
+import jakarta.annotation.Resource;
+import org.itzixi.utils.SMSUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/a")
 public class HelloController {
+    @Resource
+    SMSUtils smsUtils;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello world 88";
+    }
+
+    @GetMapping("/sms")
+    public String sms() throws Exception {
+        smsUtils.sendSMS("13691942911","9989");
+        return "send sms OK";
     }
 }
