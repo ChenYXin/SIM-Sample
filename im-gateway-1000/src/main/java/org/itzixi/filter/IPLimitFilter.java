37bf0d1e5d7fcdb9065e575798a12ab66f7adc96
Donkor <donkor@yeah.net>
2024-11-30 15:46:27 +0800

    Gateway过滤器校验token

--- im-gateway-1000/src/main/java/org/itzixi/filter/IPLimitFilter.java
+++ im-gateway-1000/src/main/java/org/itzixi/filter/IPLimitFilter.java
@@ -78,7 +78,7 @@ public class IPLimitFilter extends BaseInfoProperties implements GlobalFilter, O
         long limitLeftTimes = redis.ttl(ipRedisLimitKey);
         if (limitLeftTimes > 0) {
             // 终止请求，返回错误
-            return renderErrorMsg(exchange, ResponseStatusEnum.SYSTEM_ERROR_BLACK_IP);
+            return RenderErrorUtils.display(exchange, ResponseStatusEnum.SYSTEM_ERROR_BLACK_IP);
         }
 
         // 在redis中获得ip的累加次数
@@ -99,39 +99,12 @@ public class IPLimitFilter extends BaseInfoProperties implements GlobalFilter, O
             // 限制ip访问的时间[limitTimes]
             redis.set(ipRedisLimitKey, ipRedisLimitKey, limitTimes);
             // 终止请求，返回错误
-            return renderErrorMsg(exchange, ResponseStatusEnum.SYSTEM_ERROR_BLACK_IP);
+            return RenderErrorUtils.display(exchange, ResponseStatusEnum.SYSTEM_ERROR_BLACK_IP);
         }
 
         return chain.filter(exchange);
     }
 
-    /**
-     * 重新包装并且返回错误信息
-     *
-     * @param exchange
-     * @param responseStatusEnum
-     * @return
-     */
-    public Mono<Void> renderErrorMsg(ServerWebExchange exchange,
-                                     ResponseStatusEnum responseStatusEnum) {
-        //1.获得响应response
-        ServerHttpResponse response = exchange.getResponse();
-        //2.构建jsonResult
-        GraceJSONResult jsonResult = GraceJSONResult.exception(responseStatusEnum);
-        //3.设置header类型
-        if (!response.getHeaders().containsKey("Content-Type")) {
-            response.getHeaders().add("Content-Type",
-                    MimeTypeUtils.APPLICATION_JSON_VALUE);
-        }
-        //4.修改response的状态码code为500
-        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
-        //5.转换json并且向response中写入数据
-        String resultJson = new Gson().toJson(jsonResult);
-        DataBuffer buffer = response
-                .bufferFactory()
-                .wrap(resultJson.getBytes(StandardCharsets.UTF_8));
-        return response.writeWith(Mono.just(buffer));
-    }
 
     //过滤器的顺序，数字越小则优先级越大
     @Override
---
+++ im-gateway-1000/src/main/java/org/itzixi/filter/RenderErrorUtils.java
@@ -0,0 +1,56 @@
+package org.itzixi.filter;
+
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.itzixi.base.BaseInfoProperties;
+import org.itzixi.grace.result.GraceJSONResult;
+import org.itzixi.grace.result.ResponseStatusEnum;
+import org.itzixi.utils.IPUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+public class RenderErrorUtils extends BaseInfoProperties {
+
+    /**
+     * 重新包装并且返回错误信息
+     *
+     * @param exchange
+     * @param responseStatusEnum
+     * @return
+     */
+    public static Mono<Void> display(ServerWebExchange exchange,
+                                     ResponseStatusEnum responseStatusEnum) {
+        //1.获得响应response
+        ServerHttpResponse response = exchange.getResponse();
+        //2.构建jsonResult
+        GraceJSONResult jsonResult = GraceJSONResult.exception(responseStatusEnum);
+        //3.设置header类型
+        if (!response.getHeaders().containsKey("Content-Type")) {
+            response.getHeaders().add("Content-Type",
+                    MimeTypeUtils.APPLICATION_JSON_VALUE);
+        }
+        //4.修改response的状态码code为500
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+        //5.转换json并且向response中写入数据
+        String resultJson = new Gson().toJson(jsonResult);
+        DataBuffer buffer = response
+                .bufferFactory()
+                .wrap(resultJson.getBytes(StandardCharsets.UTF_8));
+        return response.writeWith(Mono.just(buffer));
+    }
+
+
+}
