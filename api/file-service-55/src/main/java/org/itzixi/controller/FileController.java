705a6cf7e051c9be946c22dc860b34d1b8f56e26
Donkor <donkor@yeah.net>
2024-12-01 11:41:44 +0800

    生成微信个人二维码

--- api/file-service-55/src/main/java/org/itzixi/controller/FileController.java
+++ api/file-service-55/src/main/java/org/itzixi/controller/FileController.java
@@ -10,11 +10,15 @@ import org.itzixi.grace.result.GraceJSONResult;
 import org.itzixi.grace.result.ResponseStatusEnum;
 import org.itzixi.pojo.vo.UsersVO;
 import org.itzixi.utils.JsonUtils;
+import org.itzixi.utils.QrCodeUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/file")
@@ -80,11 +84,33 @@ public class FileController {
          * 如果前端没有保存按钮则可以这么做，如果有保存提交按钮，则在前端可以触发
          * 此处则不需要进行微服务调用，让前端触发保存提交到后台进行保存
          */
-        GraceJSONResult jsonResult=userInfoMicroServiceFeign.updateFace(userId, faceUrl);
-        Object data=jsonResult.getData();
-        String json=JsonUtils.objectToJson(data);
-        UsersVO usersVO=JsonUtils.jsonToPojo(json, UsersVO.class);
+        GraceJSONResult jsonResult = userInfoMicroServiceFeign.updateFace(userId, faceUrl);
+        Object data = jsonResult.getData();
+        String json = JsonUtils.objectToJson(data);
+        UsersVO usersVO = JsonUtils.jsonToPojo(json, UsersVO.class);
 
         return GraceJSONResult.ok(usersVO);
+    }
+
+    @PostMapping("/generatorQrCode")
+    public String generatorQrCode(String wechatNumber,
+                                           String userId) throws Exception {
+        // 构建map对象
+        Map<String, String> map = new HashMap<>();
+        map.put("wechatNumber", wechatNumber);
+        map.put("userId", userId);
+        //把对象转换为json字符串，用于存储到二维码中
+        String data = JsonUtils.objectToJson(map);
+        //生成二维码
+        String qrCodePath = QrCodeUtils.generateQRCode(data);
+        //把二维码上传到MinIO
+        if (StringUtils.isNotBlank(qrCodePath)) {
+            String uuid = UUID.randomUUID().toString();
+            String objectName = "wechatNumber" + File.separator + userId + File.separator + uuid + ".png";
+            String imageQrCodeUrl=MinIOUtils.uploadFile(minIOConfig.getBucketName(), objectName, qrCodePath, true);
+            return imageQrCodeUrl;
+        }
+
+        return "";
     }
 }
