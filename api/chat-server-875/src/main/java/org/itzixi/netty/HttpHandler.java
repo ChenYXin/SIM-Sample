d5e9b8a95a9e6363b6121fc95c9e83bdf3be6b9f
Donkor <donkor@yeah.net>
2024-12-04 11:45:16 +0800

    Netty的生命周期

--- api/chat-server-875/src/main/java/org/itzixi/netty/HttpHandler.java
+++ api/chat-server-875/src/main/java/org/itzixi/netty/HttpHandler.java
@@ -14,30 +14,92 @@ import org.springframework.http.MediaType;
  * 创建自定义助手类
  */
 //SimpleChannelInboundHandler : 对应请求来说，相当于入站（入境）
-@Slf4j
+//@Slf4j
 public class HttpHandler extends SimpleChannelInboundHandler<HttpObject> {
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
         //获取channel
-        Channel channel = channelHandlerContext.channel();
+        Channel channel = ctx.channel();
 
-        //打印客户端的远程地址
-        log.info(channel.remoteAddress().toString());
-//        System.out.println(channel.remoteAddress());
+        if( msg instanceof HttpRequest ) {
+            //打印客户端的远程地址
+//        log.info(channel.remoteAddress().toString());
+            System.out.println(channel.remoteAddress());
 
-        //通过缓冲区定义发送的消息，读写数据都是通过缓冲区进行数据交换的
-        ByteBuf content = Unpooled.copiedBuffer("Hello netty", CharsetUtil.UTF_8);
+            //通过缓冲区定义发送的消息，读写数据都是通过缓冲区进行数据交换的
+            ByteBuf content = Unpooled.copiedBuffer("Hello netty", CharsetUtil.UTF_8);
 
-        //构建http的response
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
-                HttpResponseStatus.OK,
-                content);
+            //构建http的response
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
+                    HttpResponseStatus.OK,
+                    content);
 
-        //为响应添加数据类型和数据长度
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+            //为响应添加数据类型和数据长度
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
 
-        //把响应数据写到缓冲区再刷到客户端
-        channelHandlerContext.writeAndFlush(response);
+            //把响应数据写到缓冲区再刷到客户端
+            ctx.writeAndFlush(response);
+        }
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel ... 注册");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel ... 移除");
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel ... 活跃");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel ... 不活跃");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel ... 读取数据完毕");
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        System.out.println("用户事件触发");
+        super.userEventTriggered(ctx, evt);
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel ... 可写更改");
+        super.channelWritabilityChanged(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("捕获到异常");
+        super.exceptionCaught(ctx, cause);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler ... 添加");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler ... 移除");
+        super.handlerRemoved(ctx);
     }
 }
