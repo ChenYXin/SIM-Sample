769a917bc0625f3567cc1107201d28a88b81fe8c
Donkor <donkor@yeah.net>
2024-12-05 17:41:09 +0800

    RabbitMQj集成SpringBoot ，异步解耦，监听消费

--- api/main-service-66/src/main/java/org/itzixi/controller/HelloController.java
+++ api/main-service-66/src/main/java/org/itzixi/controller/HelloController.java
@@ -1,5 +1,10 @@
 package org.itzixi.controller;
 
+import jakarta.annotation.Resource;
+import org.itzixi.pojo.netty.ChatMsg;
+import org.itzixi.rabbitmq.RabbitMQTestConfig;
+import org.itzixi.utils.JsonUtils;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +15,20 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello world 66";
+    }
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/mq")
+    public String mq() {
+        ChatMsg chatMsg = new ChatMsg();
+        chatMsg.setMsg("hello rabbitmq~~");
+        String msg = JsonUtils.objectToJson(chatMsg);
+
+        rabbitTemplate.convertAndSend(RabbitMQTestConfig.TEST_EXCHANGE,
+                "imooc.wechat.test.send",
+                msg);
+        return "mq ok";
     }
 }
