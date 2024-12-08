8a9a5bfc68de2d446e85ca3fbccb3c7c51e71daf
Donkor <donkor@yeah.net>
2024-12-08 10:37:40 +0800

    手写客户端负载均衡之最少连接数

--- api/main-service-66/src/main/java/org/itzixi/controller/ChatController.java
+++ api/main-service-66/src/main/java/org/itzixi/controller/ChatController.java
@@ -1,13 +1,16 @@
 package org.itzixi.controller;
 
 import jakarta.annotation.Resource;
+import org.apache.curator.framework.CuratorFramework;
 import org.itzixi.base.BaseInfoProperties;
 import org.itzixi.grace.result.GraceJSONResult;
+import org.itzixi.pojo.netty.NettyServerNode;
 import org.itzixi.service.IChatMessagesService;
+import org.itzixi.utils.JsonUtils;
 import org.itzixi.utils.PagedGridResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/chat")
@@ -47,4 +50,30 @@ public class ChatController extends BaseInfoProperties {
         return GraceJSONResult.ok();
     }
 
+    @Resource(name = "curatorClient")
+    private CuratorFramework zkClient;
+
+    @PostMapping("/getNettyOnlineInfo")
+    public GraceJSONResult getNettyOnlineInfo() throws Exception {
+        //从zookeeper中获得当前已注册的netty服务列表
+        String path = "server-list";
+        List<String> list = zkClient.getChildren().forPath(path);
+        //System.out.println(list);
+
+        List<NettyServerNode> serverNodeList = new ArrayList<>();
+        for (String node : list) {
+            String nodeValue = new String(zkClient.getData().forPath(path + "/" + node));
+            //System.out.println(nodeValue);
+            NettyServerNode serverNode = JsonUtils.jsonToPojo(nodeValue, NettyServerNode.class);
+            serverNodeList.add(serverNode);
+        }
+        //计算当前哪个zk的node时最少连接数，获得【ip:port】并且返回给前端
+        Optional<NettyServerNode> minNodeOptional = serverNodeList.stream().min(Comparator.comparing(nettyServerNode ->
+                nettyServerNode.getOnlineCounts()
+        ));
+
+        NettyServerNode minNode = minNodeOptional.get();
+
+        return GraceJSONResult.ok(minNode);
+    }
 }
--- im-pojo/src/main/java/org/itzixi/pojo/netty/DataContent.java
+++ im-pojo/src/main/java/org/itzixi/pojo/netty/DataContent.java
@@ -11,9 +11,9 @@ import lombok.ToString;
 @ToString
 public class DataContent {
 
-//	private Integer action;			// 动作类型
-	private ChatMsg chatMsg;		// 用户的聊天内容entity
-	private String chatTime;		// 格式化后的聊天时间
-	private String extend;			// 扩展字段
-	
+    //	private Integer action;			// 动作类型
+    private ChatMsg chatMsg;        // 用户的聊天内容entity
+    private String chatTime;        // 格式化后的聊天时间
+    private String extend;            // 扩展字段
+    private NettyServerNode serverNode;
 }
